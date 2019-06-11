066942dd82f7cb2c476af9caf2b587da87dc4830
Joshua Simon <39858834+JoshuaSimon@users.noreply.github.com>
2019-06-11 15:03:47 +0200

    Added Runge Kutta method.
    I'm not sure if it works properly.

--- 2_Body_Problem.py
+++ 2_Body_Problem.py
@@ -106,6 +106,84 @@ def verlet (t, delta_t, i, v_i, R, m, G):
     return v_i_new, r_new
 
 
+def rk4 (delta_t, i, v_i, R, m, G):
+    """ Forth-order Runge Kutta method """
+
+    def a_rk(R, G, m, i, weight, r_tilde):
+        """ Special acceleration for Runge Kutta method """
+        a_new = []
+        for j in range(len(R)):
+            if i == j: continue
+            r_i = R[i][-1]
+            r_j = R[j][-1] 
+            r_ij = r_j - r_i
+            r_ij[0] = r_ij[0] + weight * r_tilde[0]
+            r_ij[1] = r_ij[1] + weight * r_tilde[1]
+
+            a_i = r_ij.elementwise(lambda x_n: G * m[j] * x_n / r_ij.norm**3)
+            a_new.append(a_i)
+        a = reduce(lambda v1, v2: v1 + v2, a_new)
+        return a
+
+    def v_tilde1(component):
+        return a_1[component] * delta_t
+
+    def r_tilde1(component):
+        return v_i[-1][component] * delta_t
+
+    def v_tilde2(component):
+        return a_2[component] * delta_t
+
+    def r_tilde2(component):
+        return (v_i[-1][component] + 0.5 * v_tilde1_new[component]) * delta_t
+
+    def v_tilde3(component):
+        return a_3[component] * delta_t
+
+    def r_tilde3(component):
+        return (v_i[-1][component] + 0.5 * v_tilde2_new[component]) * delta_t
+
+    def v_tilde4(component):
+        return a_4[component] * delta_t
+
+    def r_tilde4(component):
+        return (v_i[-1][component] + 0.5 * v_tilde3_new[component]) * delta_t
+
+    def new_v(component):
+        return v_i[-1][component] + 1/6 * v_tilde1_new[component] \
+            + 1/3 * v_tilde2_new[component] \
+            + 1/3 * v_tilde3_new[component] \
+            + 1/6 * v_tilde4_new[component]
+
+    def new_r(component):
+        return R[i][-1][component] + 1/6 * r_tilde1_new[component] \
+            + 1/3 * r_tilde2_new[component] \
+            + 1/3 * r_tilde3_new[component] \
+            + 1/6 * r_tilde4_new[component]
+
+
+    a_1 = a_nd(R, G, m, i)
+    v_tilde1_new = Vector(*[v_tilde1(component) for component in range(len(v_i[0]))])
+    r_tilde1_new = Vector(*[r_tilde1(component) for component in range(len(v_i[0]))])
+
+    a_2 = a_rk(R, G, m, i, 0.5, r_tilde1_new)
+    v_tilde2_new = Vector(*[v_tilde2(component) for component in range(len(v_i[0]))])
+    r_tilde2_new = Vector(*[r_tilde2(component) for component in range(len(v_i[0]))])
+
+    a_3 = a_rk(R, G, m, i, 0.5, r_tilde2_new)
+    v_tilde3_new = Vector(*[v_tilde3(component) for component in range(len(v_i[0]))])
+    r_tilde3_new = Vector(*[r_tilde3(component) for component in range(len(v_i[0]))])
+
+    a_4 = a_rk(R, G, m, i, 1, r_tilde3_new)
+    v_tilde4_new = Vector(*[v_tilde4(component) for component in range(len(v_i[0]))])
+    r_tilde4_new = Vector(*[r_tilde4(component) for component in range(len(v_i[0]))])
+
+    v_new = Vector(*[new_v(component) for component in range(len(v_i[0]))])
+    r_new = Vector(*[new_r(component) for component in range(len(v_i[0]))])
+
+    return v_new, r_new
+
+
 def a_nd(R, G, m, i):
     """ Acceleration of next timestep for 1 body in a system of n bodies
     Acceleration as x and y components
@@ -171,14 +249,20 @@ for t in range(0, int(t_max//delta_t)):
     for i in range(n):
         # v_i_new_e, r_i_new_e = euler(delta_t, i, V[i], R, m, G)
         # v_i_new_ec, r_i_new_ec = euler_cormer(delta_t, i, V[i], R, m, G)
-        v_i_new, r_i_new = verlet(t, delta_t, i, V[i], R, m, G)
+        v_i_new_v, r_i_new_v = verlet(t, delta_t, i, V[i], R, m, G)
+        v_i_new_rk, r_i_new_rk = rk4(delta_t, i, V[i], R, m, G)
 
         """
         print()
         print("Time = ", t, "Body = ", i)
         print("Euler vs Euler-Cormer: ", abs(r_i_new_e[0] - r_i_new_ec[0]))
         print("Euler vs Verlet: ", abs(r_i_new_e[0] - r_i_new[0]))
+        print("Verlet vs RK: ", abs(r_i_new_v[0] - r_i_new_rk[0]))
         """
+        # Choose with of the calculated values should be plotted
+        r_i_new = r_i_new_rk
+        v_i_new = v_i_new_rk
+        
         R[i].append(r_i_new)
         V[i].append(v_i_new)
 
